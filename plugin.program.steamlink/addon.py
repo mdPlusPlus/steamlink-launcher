d060960b7f4bb41aabd4b241535498a114ceff61
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2020-06-06 23:33:49 +0200

    First attempt at fixing LibreELEC installation

--- plugin.program.steamlink/addon.py
+++ plugin.program.steamlink/addon.py
@@ -8,14 +8,14 @@ __plugin__ = "Steamlink"
 __author__ = "Toast"
 __url__ = "https://github.com/swetoast/steamlink-launcher/"
 __git_url__ = "https://github.com/swetoast/steamlink-launcher/"
-__credits__ = "Ludeeus, Slouken, sgroen88, ToiletSalad"
+__credits__ = "Ludeeus, Slouken, sgroen88, ToiletSalad, mdPlusPlus"
 __version__ = "0.0.7"
 
 dialog = xbmcgui.Dialog()
 addon = xbmcaddon.Addon(id='plugin.program.steamlink')
 
 def main():
-"""Main operations of this plugin."""
+    """Main operations of this plugin."""
     create_files()
     output = os.popen("sh /tmp/steamlink-launcher.sh").read()
     dialog.ok("Starting Steamlink...", output)
@@ -23,55 +23,71 @@ def main():
 def create_files():
     """Creates bash files to be used for this plugin."""
     with open('/tmp/steamlink-launcher.sh', 'w') as outfile:
-    outfile.write("""#!/bin/sh
+        outfile.write("""#!/bin/sh
 # installation part
 install_on_libre () {
 kodi-send --action="Notification(Installing Steamlink, Please wait while installing Steamlink and packages.. this might take awhile,1500)"
 
-   mkdir -p /storage/steamlink
-   mkdir -p /storage/steamlink/overlay_work
-   mkdir -p /storage/steamlink/lib
-   mkdir -p /storage/raspbian
-   mkdir -p /storage/raspbian/lib
+mkdir -p /storage/steamlink
+mkdir -p /storage/steamlink/overlay_work
+mkdir -p /storage/steamlink/lib
+mkdir -p /storage/raspbian
+mkdir -p /storage/raspbian/image
 
-wget https://downloads.raspberrypi.org/raspbian_full_latest -O /storage/raspbian/raspbian-stretch-full.zip
-wget "$(wget -q -O - http://media.steampowered.com/steamlink/rpi/public_build.txt)" -O /storage/steamlink/steamlink.tar.gz
+## NOTE: Some raspbian/raspios images can not get extracted on LibreELEC (>2GB, "need PK compat.", etc.)
+##       We use 7za to circumvent this
+## TODO: Using a non-busybox "unzip" is probably the better alternative
+wget https://github.com/develar/7zip-bin/raw/master/linux/arm/7za -O /storage/7za
+chmod +x /storage/7za
+
+
+# Download and extract Raspberry Pi OS
+# TODO: overkill for just the libs (you currently need at least a 16GB microSD card)
+cd /storage/raspbian/
+wget http://downloads.raspberrypi.org/raspios_full_armhf/images/raspios_full_armhf-2020-05-28/2020-05-27-raspios-buster-full-armhf.zip
+/storage/7za x 2020-05-27-raspios-buster-full-armhf.zip
+
+# Download and extract Steam SteamLink
+cd /storage/
+wget "$(wget -q -O - http://media.steampowered.com/steamlink/rpi/public_build.txt)" -O /storage/steamlink.tar.gz
+tar -zxf steamlink.tar.gz
+cp /storage/steamlink/udev/rules.d/55-steamlink.rules /storage/.config/udev.rules.d/55-steamlink.rules
+rm /storage/steamlink.tar.gz
+
+# Mount Raspberry Pi OS image and extract necessary libs for Steam Link
+# TODO: overkill
+cd /storage/
+# FIXME: mount does not work under busybox -- mount: mounting /dev/loop1 on /storage/raspbian/image/ failed: Invalid argument
+mount -o loop,ro,offset=272629760 -t ext4 /storage/raspbian/2020-05-27-raspios-buster-full-armhf.img /storage/raspbian/image/
+cd /storage/raspbian/image/
+for i in libpng*.so.* libicui18n.so.* libicuuc.so.* libicudata.so.* libX11-xcb.so.* libX11.so.* libXext.so.* libxcb.so.* libxkbcommon-x11.so.* libXau.so.* libXdmcp.so.* libxcb-xkb.so.* libbsd.so.*; do cp $(find ./usr/lib/arm-linux-gnueabihf/ -type f -name $i) /storage/steamlink/lib/ ; done
+cd ../..
+umount /storage/raspbian/image
+rm -r /storage/rasbian/
+
+# Download and enable swetoast's udev rules
+cd /storage/
 wget https://raw.githubusercontent.com/swetoast/steamlink-launcher/dev/libreelec_additonal/60-steam-input.rules -O /storage/.config/system.d/storage-steamlink-udev-rules.d.mount
+systemctl enable storage-steamlink-udev-rules.d.mount
+udevadm trigger
 
-   tar -zxf /storage/steamlink/steamlink.tar.gz
-   unzip /storage/raspbian/raspbian-stretch-full.zip
-      
-   mount -o loop,ro,offset=50331648 -t ext4 /storage/raspbian/raspbian-stretch-full.zip
-   cd /storage/raspbian/lib
-   for i in libpng16.so.16 libicui18n.so.57 libicuuc.so.57 libicudata.so.57 libX11-xcb.so.1 libX11.so.6 libXext.so.6 libxcb.so.1 libxkbcommon-x11.so.0 libXau.so.6 libXdmcp.so.6 libxcb-xkb.so.1 libbsd.so.0; do cp "$(find -name $i)" .. ; done
-   cd ..
-   umount /storage/raspbian/lib
-   
-   mv /storage/raspbian/lib/* /storage/steamlink/lib
-   mv /storage/steamlink/udev/rules.d/55-steamlink.rules /storage/.config/udev.rules.d/55-steamlink.rules
-
-   systemctl enable storage-steamlink-udev-rules.d.mount
-   udevadm trigger
-   
-   rm /storage/steamlink/steamlink.tar.gz
-   rm -r /storage/rasbian/
-
+# Actually start Steam Link
 start_steamlink
 }
 
 install_on_osmc () {
 kodi-send --action="Notification(Installing Steamlink, Please wait while installing Steamlink and packages,1500)"
-sudo mv /home/osmc/.local/share/SteamLink/udev/rules.d/55-steamlink.rules /lib/udev/rules.d/55-steamlink.rules 
-   sudo apt-get install curl gnupg libc6 xz-utils -y
+sudo mv /home/osmc/.local/share/SteamLink/udev/rules.d/55-steamlink.rules /lib/udev/rules.d/55-steamlink.rules
+sudo apt-get install curl gnupg libc6 xz-utils -y
 wget http://media.steampowered.com/steamlink/rpi/steamlink.deb -O /tmp/steamlink.deb
-   sudo dpkg -i /tmp/steamlink.deb
-   rm -f /tmp/steamlink.deb
-   sudo -u osmc steamlink
+sudo dpkg -i /tmp/steamlink.deb
+rm -f /tmp/steamlink.deb
+sudo -u osmc steamlink
 start_steamlink
 }
 
 install_on_os () {
-case $(cat /etc/os-release | grep -oE "^NAME=\".*") in
+case $(cat /etc/os-release | grep -oE "^NAME=\\".*") in
  *LibreELEC*) install_on_libre ;;
       *OSMC*) install_on_osmc ;;
 esac
@@ -80,24 +96,26 @@ esac
 
 start_steamlink () {
 chmod 755 /tmp/steamlink-watchdog.sh
-case $(cat /etc/os-release | grep -oE "^NAME=\".*") in
+case $(cat /etc/os-release | grep -oE "^NAME=\\".*") in
  *LibreELEC*) su -c "nohup /tmp/steamlink-watchdog.sh >/dev/null 2>&1 &" ;;
       *OSMC*) sudo su -c "nohup sudo openvt -c 7 -s -f -l /tmp/steamlink-watchdog.sh >/dev/null 2>&1 &" ;;
-esac   
+esac
+}
 
 detect_steamlink () {
-case $(cat /etc/os-release | grep -oE "^NAME=\".*") in
+case $(cat /etc/os-release | grep -oE "^NAME=\\".*") in
  *LibreELEC*) if [ -f "/storage/steamlink/steamlink" ]; then start_steamlink ; else install_on_os; fi ;;
       *OSMC*) if [ "$(which steamlink)" -eq "1" ]; then start_steamlink ; else install_on_os; fi ;;
-esac   
+esac
 
 }
 
 detect_steamlink
 """)
+        outfile.close()
 
     with open('/tmp/steamlink-watchdog.sh', 'w') as outfile:
-    outfile.write("""#!/bin/bash
+        outfile.write("""#!/bin/bash
 # watchdog part
 watchdog_osmc () {
 sudo systemctl stop mediacenter
@@ -131,4 +149,5 @@ esac
 os_detection
 """)
         outfile.close()
+
 main()
