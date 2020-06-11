e14f2aa1f8c2ab5befe303524ecd815816238e60
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2020-06-11 19:46:00 +0200

    Add TODO

--- script.steamlink-rpi/default.py
+++ script.steamlink-rpi/default.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+#
+# TODO: Write installed version into file and for new version each start
 
 import os
 import tarfile
